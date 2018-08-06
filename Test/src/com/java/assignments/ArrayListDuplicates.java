579c573f414d083017c959bdc22a1feb04712b38
chandra <csgaddam04@gmail.com>
2018-08-06 18:29:33 -0500

    test

--- Test/src/com/java/assignments/ArrayListDuplicates.java
+++ Test/src/com/java/assignments/ArrayListDuplicates.java
@@ -15,7 +15,6 @@ public class ArrayListDuplicates {
 		l.add(9);
 		l.add(10);
 		l.add(12);
-		l.add(13);
 		ListIterator<Integer> itr = l.listIterator();
 		while(itr.hasNext()) {
 			Integer i = (Integer)itr.next();
