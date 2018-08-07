04f5ffe067ceb4f0a6c7218e21b08c88fd8200b5
chandra <csgaddam04@gmail.com>
2018-08-07 15:14:01 -0500

    test

---
+++ Test/src/com/java/practice/StaticDemo.java
@@ -0,0 +1,18 @@
+package com.java.practice;
+
+public class StaticDemo {
+	static int num = 10;
+
+	public int call() {
+		int i = num + 10;
+		return i;
+	}
+
+	public static void mian(String[] args) {
+		System.out.println(num);
+		StaticDemo d = new StaticDemo();
+		int i = d.call();
+		System.out.println(i);
+	}
+
+}
