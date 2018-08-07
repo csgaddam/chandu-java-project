ef7e6d54e70e1f8980a1bc2f8be0cf376776333e
chandra <csgaddam04@gmail.com>
2018-08-07 14:41:43 -0500

    test

--- Test/src/com/java/assignments/PalindromeString.java
+++ Test/src/com/java/assignments/PalindromeString.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class PalindromeString {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the String:");
+		Scanner scan = new Scanner(System.in);
 		String s1 = scan.next();
-		StringBuffer sb = new StringBuffer(s1);
-		String s2 = sb.reverse().toString();
-		if (s1.equals(s2))
+		scan.close();
+		if (s1.equals(new StringBuffer(s1).reverse().toString()))// code to revere the string
 			System.out.println("The given string is a palindrome");
 		else
 			System.out.println("The given String is not palindrome");
-		scan.close();
 	}
 }
--- Test/src/com/java/practice/StringDemo.java
+++ Test/src/com/java/practice/StringDemo.java
@@ -2,7 +2,12 @@ package com.java.practice;
 
 public class StringDemo {
 	public static void main(String[] args) {
-		StringBuffer sb = new StringBuffer("chandra");
+		String s1 = "chandra";
+		s1.concat("sekhar");
+		System.out.println(s1);//if the string is mutable then we should get chandra sekhar as output
+		StringBuffer sb = new StringBuffer(s1);
+		sb.append("sekhar");
+		System.out.println(sb);//StringBuffer is mutable so we got the output as chandrasekhar 
 	}
 
 }
