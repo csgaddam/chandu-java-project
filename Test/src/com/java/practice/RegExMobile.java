0a72a70d163c1a54c985ef3ca4ef2a1473a5e0c3
chandra <csgaddam04@gmail.com>
2018-08-08 15:12:37 -0500

    test

--- Test/src/com/java/practice/RegExMobile.java
+++ Test/src/com/java/practice/RegExMobile.java
@@ -5,20 +5,18 @@ import java.util.regex.*;
 
 public class RegExMobile {
 	public static void main(String[] args) {
-		Pattern p = Pattern.compile("1?[2-9]{1}[0-9]{2}[-][0-9]{3}[-][0-9]{4}");
 		System.out.println("Enter the number in the given format xxx-xxx-xxxx");
-		boolean b1 = true;
-		while(b1) {
+		while(true) {
 		Scanner scan = new Scanner(System.in);
-		boolean b = p.matcher(scan.next()).matches();
+		boolean b = Pattern.compile("1?[2-9]{1}[0-9]{2}[-][0-9]{3}[-][0-9]{4}").matcher(scan.next()).matches();
 		if(b == false) {
 			System.out.println("please enter a valid number");
 		}
 		else {
 			System.out.println("you entered a valid number");
-			b1 = false;
-			scan.close();
+			break;
 			}
+		scan.close();
 		}
 	}
 }
